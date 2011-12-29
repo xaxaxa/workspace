b47d3a35dd49622a0bf6879c54949ff040ad2297
xaxaxa <xaxaxa@xa.us.to>
2011-12-28 20:36:40 -0800

    cplib MutexLock fixes

--- cplib/headers/lock.hpp
+++ cplib/headers/lock.hpp
@@ -27,11 +27,14 @@ namespace xaxaxa
 	{
 	public:
 		pthread_mutex_t mutex;
+
 		inline MutexLock()
 		{
 			pthread_mutexattr_t attr;
+			pthread_mutexattr_init(&attr);
 			pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
 			pthread_mutex_init(&mutex, &attr);
+			pthread_mutexattr_destroy(&attr);
 		}
 		inline ~MutexLock()
 		{
