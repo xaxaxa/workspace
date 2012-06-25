4579a0b2af54b00fae33143958de25551e59c481
xaxaxa <xaxaxa@xa.us.to>
2012-06-25 00:12:55 -0700

    .

---
+++ webshell/Default.aspx.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Web;
+using System.Web.UI;
+
+namespace webshell
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		
+		public virtual void button1Clicked (object sender, EventArgs args)
+		{
+			button1.Text = "You clicked me";
+		}
+	}
+}
+
