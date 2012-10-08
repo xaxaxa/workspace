65e546b1c7e9881f9b6d5c5ed76441feeec8b0be
xaxaxa <xaxaxa@xa.us.to>
2012-10-08 15:35:09 -0700

    .

---
+++ perftests/1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define iterations 30000
+#define bufsize 300000
+int a;
+int main()
+{
+	int i,ii;
+	char* ch="";
+	int asdf=0;
+	for(i=0;i<iterations;i++) {
+		char c[bufsize];
+		for(ii=0;ii<bufsize-4;ii+=5) {
+			c[ii]='c';
+			c[ii+1]='d';
+			c[ii+2]='e';
+			c[ii+3]='a';
+			c[ii+4]='b';
+		}
+		int* ints=(int*)c;
+		for(ii=0;ii<bufsize/sizeof(int)-2;ii+=3) {
+			ints[ii+2]=ints[ii]*2+ints[ii+1]*3;
+		}
+		asdf+=ints[bufsize/sizeof(int)-1];
+	}
+	
+	printf("%i\n",asdf);
+}
+
