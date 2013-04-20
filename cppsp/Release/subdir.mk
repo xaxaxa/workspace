25720ae24b3d0b76c32fa41e38b9ff0ef670a32c
xaxaxa <xaxaxa@xa.us.to>
2013-04-20 09:51:04 -0700

    cppsp: fixed release build

--- cppsp/Release/subdir.mk
+++ cppsp/Release/subdir.mk
@@ -4,26 +4,32 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_UPPER_SRCS += \
+../common.C \
 ../cppsp_cpoll.C \
 ../cppsp_fcgi.C \
-../page.C 
+../page.C \
+../stringutils.C 
 
 OBJS += \
+./common.o \
 ./cppsp_cpoll.o \
 ./cppsp_fcgi.o \
-./page.o 
+./page.o \
+./stringutils.o 
 
 C_UPPER_DEPS += \
+./common.d \
 ./cppsp_cpoll.d \
 ./cppsp_fcgi.d \
-./page.d 
+./page.d \
+./stringutils.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.C
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../include/ -O3 -Wall -c -fmessage-length=0 --std=c++0x -Wno-pmf-conversions -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
