05c51a8aabeaa5e97bf6d57b19f20c2ff8568b83
xaxaxa <xaxaxa@xa.us.to>
2013-07-16 11:33:35 -0700

    cpoll: StringPool: align allocations

--- cpoll/cpoll.C
+++ cpoll/cpoll.C
@@ -2245,7 +2245,7 @@ namespace CP
 
 	StringPool::StringPool(int pageSize) :
 			_firstPage(NULL), _curPage(NULL), _firstRawItem(NULL), _curRawItem(NULL),
-					_pageSize(pageSize) {
+					_pageSize(pageSize), _curIndex(0) {
 
 	}
 	StringPool::~StringPool() {
