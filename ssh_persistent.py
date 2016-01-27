f6f5550826a0c5f7ff255522a0b94bf6214501b7
Xaxaxa Axaxax <xaxaxa@xa.us.to>
2016-01-27 20:59:29 +0000

    ssh_persistent: minor improvements

--- ssh_persistent.py
+++ ssh_persistent.py
@@ -6,12 +6,16 @@ if len(sys.argv)<2:
 	exit(1);
 
 
-CONNECT_WAIT=5;
-PING_INTERVAL=5;
+CONNECT_WAIT=18;
+PING_INTERVAL=3;
 PING_FAIL_INTERVAL=0;
 PING_TIMEOUT=2;
-PING_MAXFAIL=5;
-EXCEPTION_WAIT=1;
+PING_MAXFAIL=8;
+EXCEPTION_WAIT=2;
+
+def printMsg(s):
+	sys.stdout.write(s);
+	sys.stdout.flush();
 
 # timeout is in seconds
 def checkAlive(p,timeout):
@@ -24,7 +28,7 @@ def checkAlive(p,timeout):
 			if len(os.read(p.stdout.fileno(),1024))==0:
 				p.kill();
 				raise RuntimeError("checkAlive: got EOF");
-			R,W,X=select.select([p.stdout],[],[],timeout);
+			R,W,X=select.select([p.stdout],[],[],0);
 			if len(R)==0: break;
 		return True;
 	return False;
@@ -32,23 +36,29 @@ def checkAlive(p,timeout):
 while True:
 	try:
 		p=subprocess.Popen(sys.argv[1:], stdout=subprocess.PIPE,stdin=subprocess.PIPE);
-		print "started ssh";
+		printMsg("connecting... ");
+		sys.stdout.flush();
 		if not checkAlive(p,CONNECT_WAIT):
 			p.kill();
-			print "initial ping timed out";
+			print "initial ping timed out ("+str(CONNECT_WAIT)+"s)";
 			continue;
+		print "connected"
 		pingFailCnt=0;
 		while True:
 			if pingFailCnt>0: time.sleep(PING_FAIL_INTERVAL);
 			else: time.sleep(PING_INTERVAL);
 			if checkAlive(p,PING_TIMEOUT):
+				if pingFailCnt!=0:
+					print "ping succeeded";
 				pingFailCnt=0;
 			else:
 				pingFailCnt+=1;
-				print "ping failed"
+				if pingFailCnt==1:
+					printMsg("ping failed: ");
+				printMsg(str(pingFailCnt)+"/"+str(PING_MAXFAIL)+"... ");
 				if pingFailCnt>=PING_MAXFAIL:
 					p.kill();
-					print "ping max fail count exceeded";
+					print "max fail count exceeded";
 					break;
 	except:
 		print "exception occurred";
